6bb1edcbdde41cba4181754890f34bf8d6d2b281
Arnav Deshmukh <arnavdeshmukh.work@gmail.com>
2026-03-02 16:44:26 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import { fetchAuthSession } from 'aws-amplify/auth';
 
 const API_BASE_URL =
   import.meta.env.VITE_API_URL ||
-  'https://tdozbknrj8.execute-api.ap-south-1.amazonaws.com/prod';
+  'https://6iiiodbgig.execute-api.ap-south-1.amazonaws.com/prod';
 
 console.log('🔗 API Base URL:', API_BASE_URL);
 
